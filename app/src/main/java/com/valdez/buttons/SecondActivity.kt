8c84f1a7b073f9aecc447d4fb2c6c4257bdfecd5
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-03-17 12:42:36 +0800

    feat: add function to change scroll view's background color

--- app/src/main/java/com/valdez/buttons/SecondActivity.kt
+++ app/src/main/java/com/valdez/buttons/SecondActivity.kt
@@ -1,12 +1,35 @@
 package com.valdez.buttons
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.ScrollView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var layoutScrollView: ScrollView
+    private var radioButtonColorMap = mapOf(
+        R.id.radiobutton_red to R.color.red,
+        R.id.radiobutton_orange to R.color.orange,
+        R.id.radiobutton_amber to R.color.amber,
+        R.id.radiobutton_yellow to R.color.yellow,
+        R.id.radiobutton_lime to R.color.lime,
+        R.id.radiobutton_green to R.color.green,
+        R.id.radiobutton_emerald to R.color.emerald,
+        R.id.radiobutton_teal to R.color.teal,
+        R.id.radiobutton_cyan to R.color.cyan,
+        R.id.radiobutton_sky to R.color.sky,
+        R.id.radiobutton_blue to R.color.blue,
+        R.id.radiobutton_violet to R.color.violet,
+        R.id.radiobutton_purple to R.color.purple,
+        R.id.radiobutton_fuchsia to R.color.fuchsia
+    )
+    private lateinit var buttonChange: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +38,22 @@ class SecondActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        layoutScrollView = findViewById(R.id.activity_second)
+        buttonChange = findViewById(R.id.button_change)
+
+        buttonChange.setOnClickListener {
+            val selectedRadioButton =
+                radioButtonColorMap.keys.mapNotNull { findViewById<RadioButton>(it) }
+                    .firstOrNull { it.isChecked }
+
+            selectedRadioButton?.let {
+                val colorId = radioButtonColorMap[it.id]
+                colorId?.let { color ->
+                    layoutScrollView.setBackgroundColor(ContextCompat.getColor(this, color))
+                }
+            }
         }
     }
 }
